77e9c7bb6e69996093a068781683f2e1ac0967e7
Daniel Fernando Yepez Velez <54162419+DanielFernandoYepezVelez@users.noreply.github.com>
2023-10-18 21:26:34 -0500

    Update keys.js
    Actualizando Variables De Entorno

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'bk1wkxyozghdxlfx4eta-mysql.services.clever-cloud.com',
-        user: 'uuypugrxjcxmcxdu',
-        password: '5E1Rl0fhhqJU6vdMEIU8',
-        database: 'bk1wkxyozghdxlfx4eta'
+        host: process.env.HOST_DATABASE,
+        user: process.env.USER_DATABASE,
+        password: process.env.PASSWORD_DATABASE,
+        database: process.env.NAME_DATABASE
     }
 };
